715d3780c41348f6edaa6f978c6d73fc120ec623
gowshikawebronic <gowshika.webronic@gmail.com>
2025-12-11 17:42:39 +0530

    final submit

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
-  images: {
-    loader: "custom",
-    loaderFile: "./image-loader.ts",
-  },
+ 
 };
 
 export default nextConfig;
